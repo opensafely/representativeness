ab620d49d34f9e2d95665af9725580dc405aeee3
andrewscolm <colmandrews@hotmail.com>
2022-01-31 17:08:21 +0000

    change index date to July 2020

--- analysis/config.py
+++ analysis/config.py
@@ -1,3 +1,5 @@
 #study start date.  should match date in project.yaml
-index_date = "2020-01-01"
+index_date = "2020-07-01"
 end_date= "2020-12-31" 
+index_date_death = "2020-07-01"
+end_date_death= "2020-12-31" 
--- analysis/study_definition_deaths.py
+++ analysis/study_definition_deaths.py
@@ -8,7 +8,7 @@ from config import *
 # STUDY POPULATION
 
 study = StudyDefinition(
-    index_date=index_date,
+    index_date=index_date_death,
     default_expectations={
         "date": {
             "earliest": index_date,
@@ -22,7 +22,7 @@ study = StudyDefinition(
     population=patients.all(),
 
     died_any=patients.died_from_any_cause(
-        between=[index_date,end_date],
+        between=[index_date,end_date_death],
         returning="binary_flag",
         return_expectations={
             "rate" : "exponential_increase",
@@ -31,14 +31,14 @@ study = StudyDefinition(
     ),
 
     died_cause_ons=patients.died_from_any_cause(
-        between=[index_date,end_date],
+        between=[index_date,end_date_death],
         returning="underlying_cause_of_death",
         return_expectations={"category": {"ratios": {"U071":0.2, "C33":0.2, "I60":0.1, "F01":0.1 , "F02":0.05 , "I22":0.05 ,"C34":0.05, "I23":0.25}},},
     ),
 
     died_ons_covid_flag_any=patients.with_these_codes_on_death_certificate(
         covid_codelist,
-        between=[index_date,end_date],
+        between=[index_date,end_date_death],
         match_only_underlying_cause=True,
         return_expectations={
             "date": {"earliest" : "2020-01-01"},

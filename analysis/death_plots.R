e8d00cf039548133ffa7e1e3545d5f3658080fc1
andrewscolm <colmandrews@hotmail.com>
2022-02-02 14:28:46 +0000

    reorder deaths

--- analysis/death_plots.R
+++ analysis/death_plots.R
@@ -17,19 +17,31 @@ library(readr)
 
 fs::dir_create(here::here("output", "plots"))
 ############################# deaths
-
+levels<-c("Malignant neoplasm of trachea, bronchus and lung","Cerebrovascular diseases","Ischaemic heart diseases","Dementia and Alzheimer disease","COVID-19")
 death<-read_csv(here::here("output", "tables","death_count.csv"))
+
 
 death_plot<-death %>%
   filter(region!="England") %>%
-  ggplot(aes(x=Cause_of_Death, y=percentage, fill=cohort)) +geom_bar(stat = "identity",position = "dodge") + facet_wrap(~ region) +
-    theme_classic() + theme(axis.text.x = element_text(size = 16, hjust=0,vjust=0)) + coord_flip() + xlab("") + ylab(" % of all deaths")
+  ggplot(aes(x=Cause_of_Death, y=percentage, fill=cohort)) +
+  geom_bar(stat = "identity",position = "dodge") + 
+  facet_wrap(~ region) +
+  theme_classic() + 
+  theme(axis.text.x = element_text(size = 16, hjust=0,vjust=0)) + 
+  coord_flip() + 
+  xlab("") + ylab(" % of all deaths") +
+  scale_x_discrete(limits = levels)
 
 ggsave(filename=here::here("output", "plots","Cause_of_Death_count.jpg"),death_plot,width = 30, height = 30, units = "cm")
 
 death_plot_eng<-death %>%
   filter(region=="England") %>%
-  ggplot(aes(x=Cause_of_Death, y=percentage, fill=cohort)) +geom_bar(stat = "identity",position = "dodge") +
-    theme_classic() + theme(axis.text.x = element_text(size = 16, hjust=0,vjust=0)) + coord_flip() + xlab("") + ylab(" % of all deaths")
+  ggplot(aes(x=Cause_of_Death, y=percentage, fill=cohort)) +
+  geom_bar(stat = "identity",position = "dodge") +
+  theme_classic() + 
+  theme(axis.text.x = element_text(size = 16, hjust=0,vjust=0)) + 
+  coord_flip() + xlab("") + 
+  ylab(" % of all deaths") + 
+  scale_x_discrete(limits = levels)
 
 ggsave(filename=here::here("output", "plots","Cause_of_Death_count_eng.jpg"),death_plot_eng,width = 30, height = 30, units = "cm")

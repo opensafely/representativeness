95280e5aeef2d83233f43d92917d924ecb449a55
andrewscolm <73839417+andrewscolm@users.noreply.github.com>
2022-08-24 16:09:19 +0100

    add rounding

--- analysis/immortal_check.R
+++ analysis/immortal_check.R
@@ -5,10 +5,13 @@ fs::dir_create(here::here("output", "tables"))
 df_input <- read_csv(here::here("output", "cohorts","input.csv.gz"))
 
 immort<-df_input %>%
-  mutate(agerange=(case_when(age>120~"Over120",age==120~"is120",age>110~"Over110",TRUE~"Under110"))) %>%
-  count(agerange) %>%
-  mutate(n=round(n/5)*5,
-         percentage=signif(n/sum(n)*100,2))
+  mutate(agerange=(case_when(age>100~"Over120",age==120~"is120",age>90~"Over110",TRUE~"Under110"))) %>%
+  count(agerange) 
+  
+immort_rounded <- immort  %>%
+  mutate(n=case_when(n>5~n),
+         n=round(n/7)*7)
 
 
 write_csv(immort,here::here("output", "tables","immortal.csv"))
+write_csv(immort_rounded,here::here("output", "tables","immort_rounded.csv"))
